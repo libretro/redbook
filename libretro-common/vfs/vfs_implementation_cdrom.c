da05812ef38708735fdd2c65fd013af7be53cbe7
Brad Parker <cbparker@gmail.com>
2019-06-29 15:43:38 -0400

    revert

--- libretro-common/vfs/vfs_implementation_cdrom.c
+++ libretro-common/vfs/vfs_implementation_cdrom.c
@@ -147,7 +147,7 @@ void retro_vfs_file_open_cdrom(
       {
          if (!memcmp(path + 6, "-track", strlen("-track")))
          {
-            if (sscanf(path + 12, "%02hhd", &stream->cdrom.cur_track))
+            if (sscanf(path + 12, "%02u", (unsigned*)&stream->cdrom.cur_track))
             {
 #ifdef CDROM_DEBUG
                printf("CDROM: Opening track %d\n", stream->cdrom.cur_track);
@@ -227,7 +227,7 @@ void retro_vfs_file_open_cdrom(
    {
       if (!memcmp(path + 1, ":/drive-track", strlen(":/drive-track")))
       {
-         if (sscanf(path + 14, "%02hhd", &stream->cdrom.cur_track))
+         if (sscanf(path + 14, "%02u", (unsigned*)&stream->cdrom.cur_track))
          {
 #ifdef CDROM_DEBUG
             printf("CDROM: Opening track %d\n", stream->cdrom.cur_track);
@@ -262,8 +262,6 @@ void retro_vfs_file_open_cdrom(
    {
       if (!cdrom_get_inquiry(stream, model, sizeof(model)))
       {
-         size_t len = 0;
-
 #ifdef CDROM_DEBUG
          printf("CDROM Model: %s\n", model);
          fflush(stdout);
